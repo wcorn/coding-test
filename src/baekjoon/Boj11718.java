0a281f1a25811e17e06d6a5579e1dda347c35ace
wcorn <dsk08208@gmail.com>
2022-12-27 11:52:21 +0900

    Boj 11718

---
+++ src/baekjoon/Boj11718.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Boj11718 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        String str;
+        while((str = br.readLine())!=null){
+            System.out.println(str);
+        }
+    }
+}
