a743877b87a105cd93846cc81296899e9ea73d3d
peter <97747863+wcorn@users.noreply.github.com>
2023-02-02 19:58:41 +0900

    BOJ 1697 (BFS) solve

---
+++ src/baekjoon/Boj1697.java
@@ -0,0 +1,54 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Boj1697 {
+    static int[] check;
+    static void BFS(int x,int k) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(x);
+        check[x] = 0;
+
+        while(!queue.isEmpty()) {
+            int n = queue.poll();
+
+            if (check[k] != 0) break;
+
+            if(n+1 <check.length&& check[n+1] == 0) {
+                check[n+1] =check[n]+1;
+                queue.offer(n+1);
+            }
+            if( n-1>=0 && check[n-1] == 0) {
+                check[n-1] = check[n]+1;
+                queue.offer(n-1);
+            }
+            if( n*2 < check.length && check[n*2] == 0) {
+                check[n*2] = check[n]+1;
+                queue.offer(n*2);
+            }
+
+        }
+    }
+
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        check = new int[100001];
+
+        if(n == k) {
+            System.out.println(0);
+            return;
+        }
+
+        BFS(n,k);
+
+        System.out.println(check[k]);
+
+    }
+}
